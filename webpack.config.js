c0f070d371c3710a8eea41cb0f84007179b9d210
IzuEneh <izueneh21@gmail.com>
2022-12-28 14:23:24 -0700

    Add eslint with airbnb style guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,23 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-	mode: "development",
-	entry: "./src/index.js",
-	devtool: "inline-source-map",
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Todo List",
-		}),
-	],
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
